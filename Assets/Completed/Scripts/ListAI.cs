629ab0f785d64af014033456dce274a896bd7ab9
tallen12 <tallen40@gatech.edu>
2015-12-01 17:12:46 -0500

    FixedAI Simple Problems. Maybe Fixed the Microsoft.CSharp problem, if this problem occurs.

---
+++ Assets/Completed/Scripts/ListAI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using Microsoft.CSharp;
+using System;
+using System.Reflection;
+
+public class ListAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CSharpCodeProvider provider = new CSharpCodeProvider();
+        CompilerParameters parameters = new CompilerParameters();
+        foreach (string file in Directory.GetFiles("AI"))
+        {
+            parameters = new CompilerParameters();
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                parameters.ReferencedAssemblies.Add(asm.Location);
+            }
+            Debug.Log(file);
+            StreamReader theReader = new StreamReader(file, Encoding.Default);
+            string code = theReader.ReadToEnd();
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, code);
+            if (results.Errors.Count > 0)
+            {
+                foreach (CompilerError CompErr in results.Errors)
+                {
+                    Debug.Log("Line number " + CompErr.Line +
+                        ", Error Number: " + CompErr.ErrorNumber +
+                        ", '" + CompErr.ErrorText + ";");
+                }
+            }
+            Assembly assembly = results.CompiledAssembly;
+            Type program = assembly.GetType("AISimple");
+            MethodInfo main = program.GetMethod("AISimple");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
