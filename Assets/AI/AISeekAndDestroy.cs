fef9523d47e3043246f2a7bc6b21620fab9f5c7b
tallen12 <tallen40@gatech.edu>
2015-12-01 17:03:08 -0500

    Added The AI selector. Still needs some work.

---
+++ Assets/AI/AISeekAndDestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AISeekAndDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/AI/AISimple.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Completed;
+using System;
+
+public class AISimple : AIBase {
+    public override void onTurn()
+    {
+        base.onTurn();
+        foreach(Enemy e in self)
+        {
+            Vector3 cur = e.currentPos;
+            actions.Add(new AIAction(e, AIAction.Actions.Move, cur + new Vector3(1, 1, 0)));
+        }
+    }
+}
